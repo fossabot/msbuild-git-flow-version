b7aa8890461486193a3c45b15fcfb77a8338e9d3
Jarrod Davis <developer@jarrodldavis.com>
2017-07-29 11:54:21 -0500

    Start implementing Unit Tests for Version Resolver
    - Add Test Suite for Version Resolver
    - Add test for invalid arguments

---
+++ tests/JarrodDavis.GitFlowVersion.Core.Tests/VersionResolverTests.cs
@@ -0,0 +1,46 @@
+using System;
+using FluentAssertions;
+using NuGet.Versioning;
+using Xunit;
+
+namespace JarrodDavis.GitFlowVersion.Core.Tests
+{
+    public class VersionResolverTests
+    {
+        private VersionResolver _systemUnderTest;
+
+        public VersionResolverTests()
+        {
+            _systemUnderTest = new VersionResolver();
+        }
+
+        [Theory]
+        [CombinatorialData]
+        public void VersionResolverShouldThrowForInvalidArguments(
+            [CombinatorialValues(null, "", " ")]
+            string currentBranchName,
+            [CombinatorialValues(null, "", " ")]
+            string baseBranchName,
+            [SemanticVersionCombinatorialValues(null, "0.1.0-preview4", "1.0.0-rc")]
+            SemanticVersion previousStableReleaseVersion,
+            [CombinatorialValues(-10, -5, -1)]
+            int commitsSincePreviousStableVersion
+        )
+        {
+            // Arrange
+            var request = new VersionResolutionRequest
+            {
+                CurrentBranchName = currentBranchName,
+                BaseBranchName = baseBranchName,
+                PreviousStableReleaseVersion = previousStableReleaseVersion,
+                CommitsSincePreviousStableRelease = commitsSincePreviousStableVersion
+            };
+
+            // Act
+            Action action = () => _systemUnderTest.ResolveVersion(request);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>();
+        }
+    }
+}
