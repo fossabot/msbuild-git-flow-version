a2a1bbc90f91911b2db29d6cfcefe03ce9e98ecf
Jarrod Davis <developer@jarrodldavis.com>
2017-07-29 17:52:58 -0500

    Add Version Resolver invalid input tests
    Add a test case for each individual property of
    `VersionResolutionRequest` that tests invalid values.

--- src/JarrodDavis.GitFlowVersion.Core/VersionResolver.cs
+++ src/JarrodDavis.GitFlowVersion.Core/VersionResolver.cs
@@ -7,7 +7,51 @@ namespace JarrodDavis.GitFlowVersion.Core
     {
         public SemanticVersion ResolveVersion(VersionResolutionRequest request)
         {
+            ValidateRequest(request);
+
             throw new NotImplementedException();
+        }
+
+        private void ValidateRequest(VersionResolutionRequest request)
+        {
+            if (request.CurrentBranchName is null)
+            {
+                throw new ArgumentNullException(nameof(request.CurrentBranchName));
+            }
+
+            if (request.BaseBranchName is null)
+            {
+                throw new ArgumentNullException(nameof(request.BaseBranchName));
+            }
+
+            if (request.MostRecentStableReleaseVersion is null)
+            {
+                throw new ArgumentNullException(nameof(request.MostRecentStableReleaseVersion));
+            }
+
+            if (string.IsNullOrWhiteSpace(request.CurrentBranchName))
+            {
+                throw new ArgumentException("Current branch name cannot be empty or whitespace",
+                    nameof(request.CurrentBranchName));
+            }
+
+            if (string.IsNullOrWhiteSpace(request.BaseBranchName))
+            {
+                throw new ArgumentException("Base branch name cannot be empty or whitespace",
+                    nameof(request.BaseBranchName));
+            }
+
+            if (request.MostRecentStableReleaseVersion.IsPrerelease)
+            {
+                throw new ArgumentException("Stable release version cannot be a prerelease",
+                    nameof(request.MostRecentStableReleaseVersion));
+            }
+
+            if (request.CommitsSinceStableRelease < 0)
+            {
+                throw new ArgumentException("Commits since stable release must be non-negative",
+                    nameof(request.CommitsSinceStableRelease));
+            }
         }
     }
 }
--- tests/JarrodDavis.GitFlowVersion.Core.Tests/VersionResolverTests.cs
+++ tests/JarrodDavis.GitFlowVersion.Core.Tests/VersionResolverTests.cs
@@ -8,10 +8,18 @@ namespace JarrodDavis.GitFlowVersion.Core.Tests
     public class VersionResolverTests
     {
         private VersionResolver _systemUnderTest;
+        private VersionResolutionRequest _request;
 
         public VersionResolverTests()
         {
             _systemUnderTest = new VersionResolver();
+            _request = new VersionResolutionRequest
+            {
+                CurrentBranchName = "feature/add-cool-feature",
+                BaseBranchName = "develop",
+                MostRecentStableReleaseVersion = SemanticVersion.Parse("0.1.0"),
+                CommitsSinceStableRelease = 4
+            };
         }
 
         [Theory]
@@ -41,6 +49,80 @@ namespace JarrodDavis.GitFlowVersion.Core.Tests
 
             // Assert
             action.ShouldThrow<ArgumentException>();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void VersionResolverShouldThrowForInvalidCurrentBranchName(string branchName)
+        {
+            // Arrange
+            _request.BaseBranchName = branchName;
+
+            // Act
+            Action action = () => _systemUnderTest.ResolveVersion(_request);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>()
+                  .And.ParamName
+                  .Should().Be(nameof(_request.BaseBranchName));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void VersionResolverShouldThrowForInvalidBaseBranchName(string branchName)
+        {
+            // Arrange
+            _request.CurrentBranchName = branchName;
+
+            // Act
+            Action action = () => _systemUnderTest.ResolveVersion(_request);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>()
+                  .And.ParamName
+                  .Should().Be(nameof(_request.CurrentBranchName));
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("0.1.0-preview4")]
+        [InlineData("1.0.0-rc")]
+        public void VersionResolverShouldThrowForInvalidStableVersion(string semanticVersionString)
+        {
+            // Arrange
+            _request.MostRecentStableReleaseVersion = semanticVersionString is null
+                ? null
+                : SemanticVersion.Parse(semanticVersionString);
+
+            // Act
+            Action action = () => _systemUnderTest.ResolveVersion(_request);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>()
+                  .And.ParamName
+                  .Should().Be(nameof(_request.MostRecentStableReleaseVersion));
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-5)]
+        [InlineData(-10)]
+        public void VersionResolverShouldThrowForInvalidCommitCount(int commitCount)
+        {
+            // Arrange
+            _request.CommitsSinceStableRelease = commitCount;
+
+            // Act
+            Action action = () => _systemUnderTest.ResolveVersion(_request);
+
+            // Assert
+            action.ShouldThrow<ArgumentException>()
+                  .And.ParamName
+                  .Should().Be(nameof(_request.CommitsSinceStableRelease));
         }
     }
 }
