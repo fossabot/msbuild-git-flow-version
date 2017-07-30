3a7ef538343acc4963ea6ae80d62385d0c347bf9
Jarrod Davis <developer@jarrodldavis.com>
2017-07-29 22:26:06 -0500

    Add test suite for Version Resolver
    - Add `Moq` package
    - Add test suite for Version Resolver with mocked dependencies
    - Add test case for invalid arguments (i.e. not swallowing exceptions
      from the validator)

---
+++ tests/JarrodDavis.GitFlowVersion.Core.Tests/VersionResolverTests.cs
@@ -0,0 +1,40 @@
+using System;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace JarrodDavis.GitFlowVersion.Core.Tests
+{
+    public class VersionResolverTests
+    {
+        Mock<IBranchCategoryMapper> _branchCategoryMapper;
+        Mock<ILogger<VersionResolver>> _logger;
+        Mock<IVersionResolutionRequestValidator> _validator;
+
+        public VersionResolverTests()
+        {
+            _branchCategoryMapper = new Mock<IBranchCategoryMapper>();
+            _logger = new Mock<ILogger<VersionResolver>>();
+            _validator = new Mock<IVersionResolutionRequestValidator>();
+        }
+
+        [Fact]
+        public void ResolverShouldThrowForInvalidArguments()
+        {
+            // Arrange
+            _validator.Setup(validator => validator.ValidateRequest(It.IsAny<VersionResolutionRequest>()))
+                      .Throws<ArgumentException>();
+            var systemUnderTest = new VersionResolver(_branchCategoryMapper.Object,
+                                                      _logger.Object,
+                                                      _validator.Object);
+
+            // Act
+            Action action = () => systemUnderTest.ResolveVersion(new VersionResolutionRequest());
+
+            // Assert
+            action.ShouldThrow<ArgumentException>(because: "the validator threw an exception");
+            _validator.VerifyAll();
+        }
+    }
+}
